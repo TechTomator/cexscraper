bfbbb82505386ec9a6d4072970cac2ba5c7425c8
tomatolichy <tomatolichy@users.noreply.github.com>
2018-09-21 15:20:34 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -34,7 +34,10 @@ url = "http://www-news.iaea.org/EventList.aspx"
 doc_text = scraperwiki.scrape(url)
 doc = html.fromstring(doc_text)
                
-               
+for row in doc.cssselect("#tblEvents tr"):
+link_in_header = row.cssselect("h4 a").pop()
+event_title = link_in_header.text
+print event_title               
                
 # # An arbitrary query against the database
 # scraperwiki.sql.select("* from data where 'name'='peter'")
